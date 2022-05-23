2f3950fcc6cdce51a378dd521bbbad2ae22131b5
graeme <grae0026@stud.kea.dk>
2022-05-23 11:38:26 +0200

    add api call from graphcms

--- src/App.js
+++ src/App.js
@@ -1,16 +1,60 @@
-import './App.css';
-import Header from './components/header/Header';
-import Main from './components/main/Main';
-
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { request } from "graphql-request";
+import "./App.css";
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+import Page from "./pages/page/Page";
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Main/>
-      <footer></footer>
-    </div>
-  );
+	const [pages, setPages] = useState(null);
+
+	useEffect(() => {
+		const fetchPages = async () => {
+			const { pages } = await request(
+				"https://api-eu-central-1.graphcms.com/v2/cl3a5qe60007q01xmfuo8ga8x/master",
+				`
+        {
+          pages {
+            name
+            id
+            slug
+            meta {
+              title
+              description
+            }
+          }
+        }
+        `
+			);
+			setPages(pages);
+		};
+		fetchPages();
+	}, []);
+
+	return (
+		<div className="App">
+			<Header />
+			<Main />
+			<div>
+				<h1>API for pages</h1>
+				{!pages ? (
+					"Loading"
+				) : (
+					<div>
+						<ul>
+							{pages.map(({ id, name, slug }) => (
+								<li key={id}>
+									<Link to={`/pages/${slug}`}>{name}</Link>
+								</li>
+							))}
+						</ul>
+					</div>
+				)}
+			</div>
+			<footer></footer>
+		</div>
+	);
 }
 
 export default App;
