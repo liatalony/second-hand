5fa4f2e1526fdf9f8401af6ec7e265396c2a92ba
liatalony <liatalony311@gmail.com>
2022-05-23 14:08:42 +0200

    List item

--- src/components/list-item/Item.js
+++ src/components/list-item/Item.js
@@ -1,7 +1,27 @@
+import {React, useState} from 'react';
+import Heart from '../../assets/heart.svg';
+import { ReactSVG } from 'react-svg';
+import Jacket from '../../assets/jacket.jpg';
+import './item.scss';
+
 const Item =() => {
+  const [liked, setLiked] = useState(false);
+
+  function handleLike(){
+    setLiked((prev)=> !prev)
+  }
+
     return (
       <div className="Item">
-
+        <div className="item-image" style={{ backgroundImage: `url(${Jacket})`, backgroundPosition: "center", backgroundSize: "cover"}}>
+          <button onClick={handleLike} className={liked ? "item-heart-liked" : "item-heart"}>
+            <ReactSVG src={Heart}/>
+          </button>
+        </div>
+        <div className='item-detail'>
+          <h3>Item name</h3>
+          <p>kr. 100</p>
+        </div>
       </div>
     );
   }
