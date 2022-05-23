691a111e2f355cadfb1ee06c334e7e903883f20c
liatalony <liatalony311@gmail.com>
2022-05-23 11:26:22 +0200

    added burger nav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,35 @@
-import React from "react";
+import {React, useState} from "react";
+import { Link } from "react-router-dom";
+import Logo from '../../assets/logo192.png';
+
+import './navbar.scss';
 
 const Navbar = () => {
-    return (
-      <div className="navigation">
+    const [open, setOpen] = useState(false);
 
-      </div>
+    function handleNavigation(){
+        setOpen((prev)=> !prev)
+    }
+    return (    
+        <div>
+            <button className='menu--burger' onClick={handleNavigation}>
+                <img src={Logo} alt="Secont hand shop name"/>
+            </button>
+            {open &&
+            <nav>
+            <button className='menu--burger' onClick={handleNavigation}>
+                <img src={Logo} alt="Secont hand shop name"/>
+            </button>
+                <ul>
+                    <li><Link to={"/"} onClick={handleNavigation}>Home</Link></li>
+                    <li><Link to={"/men"} onClick={handleNavigation}>Men</Link></li>
+                    <li><Link to={"/women"} onClick={handleNavigation}>Women</Link></li>
+                    <li><Link to={"/about"} onClick={handleNavigation}>About</Link></li>
+                    <li><Link to={"/how-it-works"} onClick={handleNavigation}>How It Works</Link></li>
+                </ul>
+            </nav>
+            }
+        </div>    
     );
   }
   
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {React} from 'react';
 import {Link} from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
 import Logo from '../../assets/logo192.png';
@@ -6,6 +6,7 @@ import './header.scss';
 
 
 const Header = () => {
+
     return (
         <header>
             <Navbar/>
