ea946d54473d193c07822ddd263a2a3dad2426e0
graeme <grae0026@stud.kea.dk>
2022-05-23 19:13:47 +0200

    add props to main.js

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -14,7 +14,6 @@ const Main = (props) => {
 			{props.pages.map((page) => (
 				<li key={page.id}>
 					{/* <Route path={`/${page.slug}`} element={<Page />} /> */}
-					<h2>{page.name}</h2>
 				</li>
 			))}
 			<Routes>
