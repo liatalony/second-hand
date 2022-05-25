4f2f3e4f7b1d596b73b662bd130b7e836eba6d7f
graeme <grae0026@stud.kea.dk>
2022-05-25 12:20:25 +0200

    add test to home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,14 +2,14 @@ import React from "react";
 import Item from "../../components/list-item/Item";
 
 const Home = () => {
-  return (
-    <div className="Home">
-      <h1>Home</h1>
-      <div className="new-products">
-        <Item />
-      </div>
-    </div>
-  );
+	return (
+		<div className="Home">
+			<h1 data-testid="header">Home</h1>
+			<div className="new-products">
+				<Item />
+			</div>
+		</div>
+	);
 };
 
 export default Home;
---
+++ src/pages/home/__test__/Home.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Home from "../Home";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+test("blah blah blah", () => {
+	const component = render(<Home />);
+	const headerEl = component.getByTestId("header");
+	expect(headerEl.textContent).toBe("Home");
+});
