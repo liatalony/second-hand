285549cc679a3f60bc338c2da81c450b7ea2f3d8
graeme <grae0026@stud.kea.dk>
2022-05-30 09:18:46 +0200

    remove browser router from list item

--- src/components/list-item/Item.js
+++ src/components/list-item/Item.js
@@ -3,7 +3,7 @@ import Heart from "../../assets/heart.svg";
 import { ReactSVG } from "react-svg";
 import Jacket from "../../assets/jacket.jpg";
 import "./item.scss";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Item = () => {
 	const [liked, setLiked] = useState(false);
@@ -14,38 +14,36 @@ const Item = () => {
 
 	return (
 		<div className="Item">
-			<Router>
-				<div className="image-container">
-					<Link to={"/id"}>
-						<div
-							className="item-image"
-							style={{
-								backgroundImage: `url(${Jacket})`,
-								backgroundPosition: "center",
-								backgroundSize: "cover",
-							}}
+			<div className="image-container">
+				<Link to={"/id"}>
+					<div
+						className="item-image"
+						style={{
+							backgroundImage: `url(${Jacket})`,
+							backgroundPosition: "center",
+							backgroundSize: "cover",
+						}}
+					>
+						<button
+							onClick={handleLike}
+							className={liked ? "item-heart-liked" : "item-heart"}
 						>
-							<button
-								onClick={handleLike}
-								className={liked ? "item-heart-liked" : "item-heart"}
-							>
-								<ReactSVG src={Heart} />
-							</button>
-						</div>
-					</Link>
-
-					<div className="item-details">
-						<h3>
-							<Link to={"/id"}>Item name</Link>
-						</h3>
-						<p>kr. 100</p>
+							<ReactSVG src={Heart} />
+						</button>
 					</div>
-				</div>
-				<div className="item-detail">
-					<h3>Item name</h3>
+				</Link>
+
+				<div className="item-details">
+					<h3>
+						<Link to={"/id"}>Item name</Link>
+					</h3>
 					<p>kr. 100</p>
 				</div>
-			</Router>
+			</div>
+			<div className="item-detail">
+				<h3>Item name</h3>
+				<p>kr. 100</p>
+			</div>
 		</div>
 	);
 };
