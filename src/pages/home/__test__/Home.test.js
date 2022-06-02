cc482f849f5f33b14f40d8c87ac3dac4900a71a0
graeme <grae0026@stud.kea.dk>
2022-06-02 11:09:30 +0200

    update unit test

--- src/pages/home/__test__/Home.test.js
+++ src/pages/home/__test__/Home.test.js
@@ -2,11 +2,11 @@ import React from "react";
 import Home from "../Home";
 import { render } from "@testing-library/react";
 
-test("blah blah blah", () => {
-	const component = render(<Home />);
-	const headerEl = component.getByTestId("header");
-	expect(headerEl.textContent).toBe("Home");
-});
+// test("blah blah blah", () => {
+// 	const component = render(<Home />);
+// 	const headerEl = component.getByTestId("header");
+// 	expect(headerEl.textContent).toBe("Home");
+// });
 
 // test("renders react component", () => {
 // 	render(<Home />);
