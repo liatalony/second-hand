c668deb39dfd2e787401678a85db85281a9bd14a
graeme <grae0026@stud.kea.dk>
2022-06-09 20:51:41 +0200

    add status links to page top

--- src/pages/dashboard/admin/AllItems.js
+++ src/pages/dashboard/admin/AllItems.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Item from "../../../components/list-item/Item";
+import { Link } from "react-router-dom";
 import "./allItems.scss";
 
 function AllItems() {
 	return (
 		<div className="Dashboard all-items">
 			<h1>Admin - All Items</h1>
+			<div className="status-links">
+				<Link to={""}>Active</Link>
+				<Link to={""}>Pending</Link>
+				<Link to={""}>Sold</Link>
+			</div>
 			<div className="item-list-container">
 				<ul className="item-list">
 					<li>
