cc4e4855f5674c8270bb73cfcbaff8f14ec4fe34
graeme <grae0026@stud.kea.dk>
2022-06-09 21:59:14 +0200

    fix active status on links

--- src/pages/dashboard/admin/AllItems.js
+++ src/pages/dashboard/admin/AllItems.js
@@ -1,16 +1,32 @@
 import React from "react";
 import Item from "../../../components/list-item/Item";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import "./allItems.scss";
 
 function AllItems() {
+	let { status } = useParams();
 	return (
 		<div className="Dashboard all-items">
 			<h1>Admin - All Items</h1>
 			<div className="status-links">
-				<Link to={""}>Active</Link>
-				<Link to={""}>Pending</Link>
-				<Link to={""}>Sold</Link>
+				<Link
+					to={"/dashboard/items/active"}
+					className={status == "active" ? "active" : ""}
+				>
+					Active
+				</Link>
+				<Link
+					to={"/dashboard/items/pending"}
+					className={status == "pending" ? "active" : ""}
+				>
+					Pending
+				</Link>
+				<Link
+					to={"/dashboard/items/sold"}
+					className={status == "sold" ? "active" : ""}
+				>
+					Sold
+				</Link>
 			</div>
 			<div className="item-list-container">
 				<ul className="item-list">
