a29eb4b3c23c088e66ae7a2a03f0f823e752ec30
graeme <grae0026@stud.kea.dk>
2022-06-09 20:44:34 +0200

    add items list to page

--- src/pages/dashboard/admin/AllItems.js
+++ src/pages/dashboard/admin/AllItems.js
@@ -1,11 +1,33 @@
 import React from "react";
-
+import Item from "../../../components/list-item/Item";
+import "./allItems.scss";
 
 function AllItems() {
-
 	return (
-		<div className="Dashboard">
+		<div className="Dashboard all-items">
 			<h1>Admin - All Items</h1>
+			<div className="item-list-container">
+				<ul className="item-list">
+					<li>
+						<Item />
+					</li>
+					<li>
+						<Item />
+					</li>
+					<li>
+						<Item />
+					</li>
+					<li>
+						<Item />
+					</li>
+					<li>
+						<Item />
+					</li>
+					<li>
+						<Item />
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 }
