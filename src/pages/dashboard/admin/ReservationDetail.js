3d795dd96e84f64bdef35dc59fb75d2330ce5067
graeme <grae0026@stud.kea.dk>
2022-06-09 22:28:26 +0200

    add single reservation item page

--- src/pages/dashboard/admin/ReservationDetail.js
+++ src/pages/dashboard/admin/ReservationDetail.js
@@ -1,11 +1,32 @@
 import React from "react";
-
+import Item from "../../../components/list-item/Item";
 
 function ReservationDetails() {
-
 	return (
-		<div className="Dashboard">
+		<div className="Dashboard all-items">
 			<h1>Admin - Reservation Details</h1>
+			<div className="item-list-container">
+				<ul className="item-list">
+					<li>
+						<Item />
+					</li>
+					<li>
+						<Item />
+					</li>
+					<li>
+						<Item />
+					</li>
+					<li>
+						<Item />
+					</li>
+					<li>
+						<Item />
+					</li>
+					<li>
+						<Item />
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 }
