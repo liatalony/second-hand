bfadc709a402e3b3201cb9705817cda6638da220
graeme <grae0026@stud.kea.dk>
2022-06-09 20:04:54 +0200

    add account details form

---
+++ src/pages/dashboard/AccountDetails.js
@@ -0,0 +1,12 @@
+import { React } from "react";
+import { AccountForm } from "../../components/forms/Forms";
+
+function AccountDetails() {
+	return (
+		<div>
+			<AccountForm />
+		</div>
+	);
+}
+
+export default AccountDetails;
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
+import AccountDetails from "./AccountDetails";
 import ReservationDetails from "./admin/ReservationDetail";
 import ReservationsList from "./admin/Reservations";
 import AllItems from "./admin/AllItems";
@@ -9,11 +10,11 @@ import "./dashboard.scss";
 import DashboardHeader from "../../components/header/dashboardHeader";
 
 function Dashboard() {
-
 	return (
 		<div className="Dashboard">
-			<DashboardHeader/>
+			<DashboardHeader />
 			<h1>Dashboard</h1>
+			<Link to={"/dashboard/account-details"}>Account</Link>
 			<Link to={"/dashboard/my-items"}>My Items</Link>
 			<Link to={"/dashboard/my-items/add"}>Add item</Link>
 			<Link to={"/dashboard/my-items/edit/:id"}>One item</Link>
@@ -24,13 +25,14 @@ function Dashboard() {
 
 			<Routes>
 				<Route path={"/"} element={<MyItems />} />
+				<Route path={"/account-details"} element={<AccountDetails />} />
 				<Route path={"/my-items"} element={<MyItems />} />
 				<Route path={"/my-items/add"} element={<ItemForm />} />
 				<Route path={"/my-items/edit/:id"} element={<ItemForm />} />
 				<Route path={"/items"} element={<AllItems />} />
 				<Route path={"/items/:id"} element={<ItemForm />} />
-				<Route path={"/reservations"} element={<ReservationsList />} />			
-				<Route path={"/reservations/:id"} element={<ReservationDetails />} />	
+				<Route path={"/reservations"} element={<ReservationsList />} />
+				<Route path={"/reservations/:id"} element={<ReservationDetails />} />
 			</Routes>
 		</div>
 	);
