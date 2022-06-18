ab6fe7c9a88b6e205e8622847ca1817c63411268
liatalony <liatalony311@gmail.com>
2022-06-18 15:44:22 +0200

    admin can approve or cancel reservations

--- src/pages/dashboard/admin/ReservationDetail.js
+++ src/pages/dashboard/admin/ReservationDetail.js
@@ -1,31 +1,40 @@
-import React from "react";
-// import Item from "../../../components/list-item/Item";
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { axiosPrivate } from "../../../api/axios";
+import Item from "../../../components/list-item/Item";
 
 function ReservationDetails() {
+	const [itemList, setItemList] = useState(false);
+	const {id} = useParams();
+	const navigate = useNavigate();
+	useEffect(()=>{
+		axiosPrivate.get(`/products/reservations/${id}`).then(res=> setItemList(res.data));
+	},[])
+
+	const handleStatus = (status) => {
+		axiosPrivate.get(`/products/reservations/${id}/${status}`).then(res=> setItemList(res.data)).then(()=>{navigate("/dashboard/reservations");});
+		
+	}
+
 	return (
 		<div className="Dashboard all-items">
 			<h1>Admin - Reservation Details</h1>
 			<div className="item-list-container">
 				<ul className="item-list">
-					{/* <li>
-						<Item />
-					</li>
-					<li>
-						<Item />
-					</li>
-					<li>
-						<Item />
-					</li>
-					<li>
-						<Item />
-					</li>
-					<li>
-						<Item />
-					</li>
-					<li>
-						<Item />
-					</li> */}
+					{!itemList ? <p>No items in the bag</p> : (
+						itemList.map(item => {
+							return <Item key={"item" + item.product_id} details={item} />
+						})
+					)}
 				</ul>
+			</div>
+			<div className="container--buy--desktop">
+				<div className="button">
+					<button className="btn btn--primary" onClick={()=>handleStatus("approved")}>Approve</button>
+				</div>
+				<div className="button">
+					<button className="btn btn--primary" onClick={()=>handleStatus("cancelled")}>Cancel</button>
+				</div>
 			</div>
 			<section className="container--buy">
 				<div className="buy">
@@ -34,7 +43,10 @@ function ReservationDetails() {
 						<h1 className="price">kr. 100</h1>
 					</div>
 					<div className="button">
-						<button className="btn btn--primary">Confirm bag</button>
+						<button className="btn btn--primary"  onClick={()=>handleStatus("approved")}>Approve</button>
+					</div>
+					<div className="button">
+						<button className="btn btn--primary" onClick={()=>handleStatus("cancelled")}>Cancel</button>
 					</div>
 				</div>
 			</section>
--- src/pages/dashboard/admin/Reservations.js
+++ src/pages/dashboard/admin/Reservations.js
@@ -1,62 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { ReactSVG } from "react-svg";
+import axiosPrivate from "../../../api/axios";
 import Check from "../../../assets/icons/check-outline.svg";
 import Dots from "../../../assets/icons/dots-pending-filled.svg";
 import Cross from "../../../assets/icons/cross-cancel.svg";
 import "./reservationsAdmin.scss";
 
-function ReservationsList() {
-	const orderNumber = 2123123;
-	const orderStatus = "accepted";
+const ReservationsList = () => {
+	const [reservation, setReservation] = useState(false);
+	// const orderNumber = 2123123;
+	// const orderStatus = "accepted";
+
+	useEffect(()=>{
+		try {
+			axiosPrivate.get("/products/reservations").then(res=> setReservation(res.data));
+		} catch (error) {
+			console.log(error.message);
+		}
+	},[])
 
 	return (
 		<div className="Dashboard all-items">
 			<h1 className="title">Admin - Reservations</h1>
 			<div className="item-list-container">
 				<ul className="item-list">
-					<li>
-						<Link to={"/dashboard/reservations/:id"} className="order">
-							<div className="order-contents">
-								<div className="order-icon">
-									<ReactSVG src={Check} />
-								</div>
-								<div className="order-text">
-									<h2>Order number</h2>
-									<h3>{orderNumber}</h3>
-									<p>{orderStatus}</p>
-								</div>
-							</div>
-						</Link>
-					</li>
-					<li>
-						<Link to={"/dashboard/reservations/:id"} className="order">
-							<div className="order-contents">
-								<div className="order-icon">
-									<ReactSVG src={Dots} />
-								</div>
-								<div className="order-text">
-									<h2>Order number</h2>
-									<h3>{orderNumber}</h3>
-									<p>{orderStatus}</p>
-								</div>
-							</div>
-						</Link>
-					</li>
-					<li>
-						<Link to={"/dashboard/reservations/:id"} className="order">
-							<div className="order-contents">
-								<div className="order-icon">
-									<ReactSVG src={Cross} />
-								</div>
-								<div className="order-text">
-									<h2>Order number</h2>
-									<h3>{orderNumber}</h3>
-									<p>{orderStatus}</p>
-								</div>
-							</div>
-						</Link>
-					</li>
+					{!reservation ? <p>No reservations yet...</p> : (
+						reservation.map(res =>{
+							return (
+								<li  key={res.reservation_id}>
+									<Link to={"/dashboard/reservations/" +res.reservation_id} className="order">
+										<div className="order-contents">
+											<div className="order-icon">
+												<ReactSVG src={res.status=="approved" ? Check : res.status=="pending" ? Dots : Cross} />
+											</div>
+											<div className="order-text">
+												<h2>#{res.reservation_id}</h2>
+												<h3>{res.user_email}</h3>
+												<p>{res.status}</p>
+											</div>
+										</div>
+									</Link>
+								</li>
+
+							)
+						})
+					)}
 				</ul>
 			</div>
 		</div>
