688a805d78ba26d76a9d3a466028feee9cf50b27
graeme <grae0026@stud.kea.dk>
2022-06-19 19:04:28 +0200

    fix banner image homepage

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Item from "../../components/list-item/Item";
 import "./home.scss";
-import Banner from "../../assets/banner-six.jpeg";
+import Banner from "../../assets/banner-new-1-small.jpg";
 import { Link } from "react-router-dom";
 import axios from "../../api/axios";
 
@@ -28,8 +28,9 @@ const Home = () => {
 			<div
 				className="banner"
 				style={{
-					backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,200,0.2)), url(${Banner})`,
+					backgroundImage: `linear-gradient(rgba(0,0,0,0.4), rgba(0,0,200,0.2)), url(${Banner})`,
 					backgroundSize: "cover",
+					backgroundPosition: "top",
 				}}
 			>
 				<h1 className="title">No planet B</h1>
@@ -37,10 +38,10 @@ const Home = () => {
 					Browse and reserve our second hand items before you come to our shop
 				</p>
 				<div className="buttons">
-					<Link to={"/shop/women"} className={"btn btn--primary"}>
+					<Link to={"/shop/women"} className={"btn btn--primary btn--banner"}>
 						Shop Women
 					</Link>
-					<Link to={"/shop/men"} className={"btn btn--primary"}>
+					<Link to={"/shop/men"} className={"btn btn--primary btn--banner"}>
 						Shop Men
 					</Link>
 				</div>
